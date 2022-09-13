0e892502440a7767b42527cf69d8fbaad232e6f9
Mustafa BİNGÜL <85060597+BingulHan@users.noreply.github.com>
2022-09-13 14:41:03 +0300

    first commit

---
+++ dev/bingulhan/day1.go
@@ -0,0 +1,14 @@
+//Today I showed you how to output successfully and how to main simple syntax.
+//Bugün dışarıya başarılı çıktısı nasıl verdirilir ve ana basit syntax nasıl onu gösterdim.
+
+package main
+
+import "fmt"
+
+//is the function from which the application starts.
+func main(){
+
+    fmt.Println("successful")
+
+	
+}
