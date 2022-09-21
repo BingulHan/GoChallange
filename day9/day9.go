072f8ff6310c08fdc3201ce09f0f0502da50e0d5
Mustafa BİNGÜL <85060597+BingulHan@users.noreply.github.com>
2022-09-21 12:09:39 +0300

    Day 9 added

---
+++ day9/day9.go
@@ -0,0 +1,18 @@
+//We learned maps today!
+//Bugün mapleri öğrendik!
+
+package main
+
+import  (
+	"fmt" )
+
+//is the function from which the application starts.
+func main(){
+
+	city := map[string]int{"İstanbul": 18000000, "Ankara": 600000}
+
+	fmt.Printf("\t%v\n", city)
+
+
+
+}
